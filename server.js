7a3e0ebafaa8f0830cde9b35506c1f561cd45abc
Christoph <34290975+Soliseum@users.noreply.github.com>
2021-03-05 16:08:09 -0800

    fixing db connection

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const db = knex({
 	client     : 'pg',
 	connection : {
 		connectionString : process.env.DATABASE_URL, //localhost
-		ssl              : true,
+		ssl              : {
+			rejectUnauthorized : false,
+		},
 	},
 });
 
